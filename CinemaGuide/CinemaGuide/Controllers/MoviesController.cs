ccd9c3399fcdcc46597b0e62464cf77c5aea0cb7
Unknown <r.artem74@gmail.com>
2018-04-09 16:49:02 +0500

    create search controller, add di and settings

---
+++ CinemaGuide/CinemaGuide/Controllers/MoviesController.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using CinemaGuide.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaGuide.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly MoviesAggregator moviesAggregator;
+
+        public MoviesController(MoviesAggregator moviesAggregator)
+        {
+            this.moviesAggregator = moviesAggregator;
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> Search(string query)
+        {
+            var result = await moviesAggregator.SearchAsync(query);
+            return new JsonResult(result);
+        }
+    }
+}
--- CinemaGuide/CinemaGuide/Models/ErrorViewModel.cs
+++ CinemaGuide/CinemaGuide/Models/ErrorViewModel.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace CinemaGuide.Models
 {
     public class ErrorViewModel
