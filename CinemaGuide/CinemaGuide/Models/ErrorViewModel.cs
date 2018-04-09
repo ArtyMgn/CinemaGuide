6d45724cc93783c2cfd99c5ab35c362bb5f0988f
Alexander Satov <aleksandr-satov@mail.ru>
2018-04-09 18:30:48 +0500

    remove error model

--- CinemaGuide/CinemaGuide/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CinemaGuide.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
