27e9d7fbfce11068e1cca9b02d79608d54286746
ArtyMgn <r.artem74@gmail.com>
2018-05-18 01:44:05 +0500

    add db context and make first migrations

---
+++ CinemaGuide/CinemaGuide/Models/ApplicationContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CinemaGuide.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<DbUser> Users { get; set; }
+        public DbSet<DbProfile> Profiles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DbUser>()
+                .HasOne(user => user.Profile)
+                .WithOne(profile => profile.User)
+                .HasForeignKey<DbUser>(x => x.ProfileForeignKey);
+        }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
