a097df82f03b0a6ecffc13163fc0b2be93811b7a
ArtyMgn <r.artem74@gmail.com>
2018-05-21 05:45:46 +0500

    add "IsConfirmed" column

--- CinemaGuide/CinemaGuide/Migrations/20180520003934_UserConfirm.Designer.cs
+++ CinemaGuide/CinemaGuide/Migrations/20180520003934_UserConfirm.Designer.cs
@@ -4,16 +4,14 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
-using Microsoft.EntityFrameworkCore.Storage;
-using Microsoft.EntityFrameworkCore.Storage.Internal;
-using System;
 
 namespace CinemaGuide.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20180520003934_UserConfirm")]
+    partial class UserConfirm
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
