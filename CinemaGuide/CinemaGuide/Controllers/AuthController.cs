150e5b75787865fe2b5b8f0f5322edbcda9b8e98
ArtyMgn <r.artem74@gmail.com>
2018-05-21 05:50:23 +0500

    add login, signUp, account confirmation, password reset

--- CinemaGuide/CinemaGuide/Controllers/AuthController.cs
+++ CinemaGuide/CinemaGuide/Controllers/AuthController.cs
@@ -7,6 +7,7 @@ using CinemaGuide.Models.Db;
 using CinemaGuide.Utils;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,9 +16,11 @@ namespace CinemaGuide.Controllers
     public class AuthController : Controller
     {
         private readonly ApplicationContext context;
+        private readonly MailClient mailClient;
 
-        public AuthController(ApplicationContext context)
+        public AuthController(ApplicationContext context, MailClient mailClient)
         {
+            this.mailClient = mailClient;
             this.context = context;
         }
 
@@ -33,41 +36,131 @@ namespace CinemaGuide.Controllers
             return View(profile);
         }
 
+        [HttpGet("/auth/forgot_password")]
+        public IActionResult ForgotPassword([FromServices] Profile profile)
+        {
+            return View(profile);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Confirm(string login, string token)
+        {
+            var user = await TryGetUser(login);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var expectedToken = PasswordEncryptor.GenerateHash($"{user.Id}{login}");
+
+            if (expectedToken != token)
+            {
+                return Forbid();
+            }
+
+            user.IsConfirmed = true;
+            context.Update(user);
+            await context.SaveChangesAsync();
+
+            await SetAuthCookie(user.Id, user.Login);
+            await mailClient.SendGreeting(user);
+            return RedirectToAction("Index", "Home");
+        }
+
         [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> UpdatePassword(string login, string password, string confirmPassword)
+        {
+            var dbUser = await TryGetUser(login);
+            var salt = PasswordEncryptor.GenerateSalt();
+            dbUser.Salt = Convert.ToBase64String(salt);
+            dbUser.Password = PasswordEncryptor.GenerateHash(password, salt);
+            context.Update(dbUser);
+            await context.SaveChangesAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ResetPassword([FromServices] Profile profile, string login, string token)
+        {
+            var user = await TryGetUser(login);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var expectedToken = PasswordEncryptor.GenerateHash($"{user.Id}{login}");
+
+            if (expectedToken != token)
+            {
+                return Forbid();
+            }
+
+            profile.User = user;
+            profile.User.Password = string.Empty;
+            return View(profile);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ResetPassword([FromServices] Profile profile, string login)
+        {
+            var user = await TryGetUser(login);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+        
+            await mailClient.SendResetPasswordLink(user);
+            return View("PasswordResetInstruction", profile);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> SignUp([FromServices] Profile profile, User user)
         {
+            if (await context.Users.AnyAsync(x => x.Email == user.Email))
+            {
+                ModelState.AddModelError("User.Email", "к этой почте уже привязан существующий пользователь");
+                return View(profile);
+            }
+
             var dbUser = await TryGetUser(user.Login);
 
+            profile.User = user;
             if (dbUser != null)
             {
                 ModelState.AddModelError("UserCredentials.Login",
                     "Пользователь с таким логином уже существует");
-                profile.User = user;
-
                 return View(profile);
             }
 
             var salt = PasswordEncryptor.GenerateSalt();
 
-            user.Role = "user";
-            user.Salt = Convert.ToBase64String(salt);
-            user.Password = PasswordEncryptor.GenerateHash(user.Password, salt);
+            profile.User.Role = "user";
+            profile.User.Salt = Convert.ToBase64String(salt);
+            profile.User.Password = PasswordEncryptor.GenerateHash(user.Password, salt);
 
-            context.Add(user);
+            var entityEntry = context.Add(user);
             context.SaveChanges();
 
-            return RedirectToAction("Login", "Auth");
+            await mailClient.SendConfirmLink(entityEntry.Entity);
+
+            return View("Сonfirmation", profile); ;
         }
 
         [HttpGet]
+        [Authorize]
         public async Task<IActionResult> Logout()
         {
             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-
             return RedirectToAction("Index", "Home");
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> Login([FromServices] Profile profile, Credentials credentials)
         {
             var user = await TryGetUser(credentials.Login);
@@ -81,6 +174,12 @@ namespace CinemaGuide.Controllers
                 return View(profile);
             }
 
+            if (!user.IsConfirmed)
+            {
+                ModelState.AddModelError("Credentials.Login", "учетная запись не подтверждена");
+                return View(profile);
+            }
+
             if (!PasswordEncryptor.IsEqualPasswords(user.Password, user.Salt, credentials.Password))
             {
                 ModelState.AddModelError("Credentials.Password", "Неверный пароль");
@@ -88,19 +187,24 @@ namespace CinemaGuide.Controllers
                 return View(profile);
             }
 
+            await SetAuthCookie(user.Id, credentials.Login);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        private Task SetAuthCookie(int userId, string login)
+        {
             var claims = new List<Claim>
             {
-                new Claim(ClaimTypes.NameIdentifier, $"{user.Id}"),
-                new Claim(ClaimsIdentity.DefaultNameClaimType, credentials.Login)
+                new Claim(ClaimTypes.NameIdentifier, $"{userId}"),
+                new Claim(ClaimsIdentity.DefaultNameClaimType, login)
             };
 
             var id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType,
                 ClaimsIdentity.DefaultRoleClaimType);
 
-            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+            return HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
                 new ClaimsPrincipal(id));
-
-            return RedirectToAction("Index", "Home");
         }
 
         private async Task<User> TryGetUser(string login)
